153b85a26bdb9a616e8f305d962ad3e256fa77a7
Halim Choi <hchoicareer@gmail.com>
2023-03-23 10:59:19 -0400

    completed lab

--- src/index.js
+++ src/index.js
@@ -1,3 +1,43 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  const form = document.getElementById("create-task-form");
+  const submitBtn = document.getElementById("create-button");
+
+  form.addEventListener("submit", function (event) {
+    event.preventDefault()
+  });
+
+  submitBtn.addEventListener("click", () => {
+    let textInput = document.getElementById("new-task-description").value;
+
+    if (textInput != "") {
+      addList(textInput);
+    }
+    else {
+      alert("Please enter a value.")
+    }
+    textInput = "";
+  })
+
+
+  function addList(text) {
+    let taskList = document.getElementById("tasks");
+    let newItem = document.createElement("li");
+    newItem.textContent = text;
+    taskList.appendChild(newItem);
+    // createDeleteBtn()
+  }
+
+  // function createDeleteBtn() {
+  //   let deleteBtn = document.createElement("button");
+  //   let taskList = document.getElementById("tasks");
+  //   deleteBtn.setAttribute = ("id", "delete-btn");
+  //   deleteBtn.innerText = "Delete Task";
+  //   taskList.appendChild(deleteBtn);
+
+  //   deleteBtn.addEventListener("click", () => {
+  //     let taskList = document.getElementById("tasks");
+  //     taskList.remove(taskList);
+      
+  //   })
+  // }
 });
